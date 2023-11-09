1092590e5f7689f93b21c259ce281ad8411877eb
jphillips1833 <justin.phillips1833@gmail.com>
2023-11-09 10:34:29 -0600

    update to seperate js

---
+++ location.js
@@ -0,0 +1,38 @@
+// location.js
+
+const map = L.map("map").setView([0, 0], 2);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+
+let marker;
+
+function centerMap(latitude, longitude) {
+  map.setView([latitude, longitude], 13);
+}
+
+function addMarker(latitude, longitude) {
+  if (marker) {
+    map.removeLayer(marker);
+  }
+  marker = L.marker([latitude, longitude]).addTo(map);
+}
+
+function shareLocation() {
+  const watchId = navigator.geolocation.watchPosition(
+    (pos) => {
+      const { latitude, longitude } = pos.coords;
+
+      // Call functions from location.js to center the map and add a marker
+      centerMap(latitude, longitude);
+      addMarker(latitude, longitude);
+    },
+    (err) => {
+      console.error("Error getting current location:", err);
+    }
+  );
+
+  return watchId;
+}
+
+function stopSharingLocation(watchId) {
+  navigator.geolocation.clearWatch(watchId);
+}
